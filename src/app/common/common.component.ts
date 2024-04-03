7aa5a4ed8bfbd539676c36026d2f62b836d16919
Yuvraj Dhamdhere <yuvraj.10712688@ltimindtree.com>
2024-04-03 20:03:49 +0530

    New blogs ng project files

---
+++ src/app/common/common.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-common',
+  standalone: true,
+  imports: [],
+  templateUrl: './common.component.html',
+  styleUrl: './common.component.scss'
+})
+export class CommonComponent {
+
+}
